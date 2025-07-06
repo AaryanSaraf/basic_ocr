a1b4755cc7dbc481a6d1e4320140622b8e17677d
Aaryan Saraf <aaryansaraf891@gmail.com>
2025-07-06 22:36:55 +0530

    Initial commit: OCR + Gemini LLM pipeline

---
+++ code.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import pytesseract
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBFNVIPkou4BbwASnc8GAVvRHsLLjK3Aeg")
+model=genai.GenerativeModel("gemini-2.5-pro")
+img = Image.open('invoice.png')
+text = pytesseract.image_to_string(img)
+print(text)
+
+question=input("What would you like to ask about this document?\n")
+response = model.generate_content([
+    f"Document:\n{text}",
+    question
+])
+print("\nAnswer:\n", response.text)
+
+
